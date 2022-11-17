66970f1931d5a627f02aeed174578a64cce85d81
Dane Leube <dane.leube@baloise.ch>
2022-11-17 17:12:36 +0100

    initial import

---
+++ src/main/kotlin/com/tt/tournament/registration/TournamentRegistrationApplication.kt
@@ -0,0 +1,11 @@
+package com.tt.tournament.registration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TournamentRegistrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<TournamentRegistrationApplication>(*args)
+}
---
+++ src/test/kotlin/com/tt/tournament/registration/TournamentRegistrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tt.tournament.registration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TournamentRegistrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
