f99949e956ed60428752e0ec000d47349dc47606
Dane Leube <dane.leube@3ap.ch>
2025-12-08 19:38:53 +0100

    feat: Add player list

--- src/main/kotlin/com/tt/tournament/infrastructure/db/PlayerRepository.kt
+++ src/main/kotlin/com/tt/tournament/infrastructure/db/PlayerRepository.kt
@@ -73,6 +73,27 @@ class PlayerRepository(val jdbcClient: JdbcClient) {
         return resultMap
     }
 
+    fun readAllPlayers() : List<Player> {
+        val sql = "SELECT P.Play_FirstName, P.Play_LastName,  t.Type_Name, tp.typl_paid, c.Club_Name, c.Club_AdresseOrt, t.Type_Name, t.Type_ID, t.Type_StartGebuehr, P.Play_ID, tp.typl_paid" +
+                    "                FROM typeperplayer tp, player P, type t, club c " +
+                    "                where tp.typl_play_id = P.Play_ID AND t.Type_ID = tp.typl_type_id" +
+                    "                and c.Club_ID = P.Play_Club_ID" +
+                    "                order by P.Play_LastName, P.Play_FirstName"
+
+        val result = jdbcClient.sql(sql)
+            .query(this::mapToPlayer)
+        val resultMap = HashMap<Number, Player>()
+        for (player in result) {
+            if(resultMap.contains(player.id)) {
+                val existingEntry = resultMap[player.id]
+                existingEntry?.addDisciplines(player.disciplines)
+                continue
+            }
+            resultMap[player.id] = player
+        }
+        return resultMap.values.toList()
+    }
+
 
 
     fun mapToPlayer(resultSet: ResultSet) : List<Player> {
